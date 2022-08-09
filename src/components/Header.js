0b21c6c062bff19c6f2981f0e36b7da5d482f7b3
Akash Kumar sahoo <36466687+akashfit2max@users.noreply.github.com>
2022-08-09 18:01:02 +0530

    add redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@mui/icons-material/Menu';
-
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function Header() {
+
+  const [burgerStatus, setBurgerStatus] = useState(false);
+
   return (
     <Container>
       <a>
@@ -19,11 +22,23 @@ export default function Header() {
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-        <CustomMenu />
+        <CustomMenu onClick={() => setBurgerStatus(true)} />
       </RightMenu>
 
-      <BurgerNav>
-        <li><a href="#">Ex</a></li>
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          <CloseButton onClick={() => setBurgerStatus(false)} />
+        </CloseWrapper>
+        <li><a href="#">Existing Inventory</a></li>
+        <li><a href="#">Used Inventory</a></li>
+        <li><a href="#">Trade-in</a></li>
+        <li><a href="#">Cybertruck</a></li>
+        <li><a href="#">Roadaster</a></li>
+        <li><a href="#">Model X</a></li>
+        <li><a href="#">Model Y</a></li>
+        <li><a href="#">Model 3</a></li>
+        <li><a href="#">Model S</a></li>
+
       </BurgerNav>
 
     </Container>
@@ -40,6 +55,7 @@ const Container = styled.div`
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
 `
 
 
@@ -79,4 +95,37 @@ const CustomMenu = styled(MenuIcon)`
 
 const BurgerNav = styled.div`
 
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 16;
+  list-style: none;
+  padding: 20px;
+  display: column;
+  text-align: start;
+  transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+  transition: transform 0.2s ease-in;
+
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0,0,0,.2);
+
+    a {
+      font-weight: 600;
+    }
+
+  }
+`
+
+const CloseButton = styled(CloseIcon)`
+  cursor: pointer;
+
+`
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `
