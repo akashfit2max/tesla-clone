7efda5744eeaf8dafa64ec3b553a4299c1b8ecfb
Akash Kumar sahoo <36466687+akashfit2max@users.noreply.github.com>
2022-08-09 13:56:14 +0530

    design complete

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
 
-function Section() {
+function Section(props) {
   return (
     <div>
-      <Warp>
+      <Warp bgImage={props.backgroundImg}>
         <ItemText>
-          <h1>Model S</h1>
-          <p>Order Online Touchless Delivery</p>
+          <h1>{props.title}</h1>
+          <p>{props.description}</p>
         </ItemText>
+        <Button>
+          <ButtonGroup>
+            <LeftButton>
+              {props.leftBtnText}
+            </LeftButton>
+            {
+              props.rightBtnText &&
+              <RightButton>
+                {props.rightBtnText}
+              </RightButton>
+            }
 
-        <ButtonGroup>
-          <LeftButton>
-            Custom Order
-          </LeftButton>
-          <RightButton>
-            Existing Inventory
-          </RightButton>
-        </ButtonGroup>
-
-        <DownArrow src='/images/down-arrow.svg' />
+          </ButtonGroup>
+          <DownArrow src="/images/down-arrow.svg" />
+        </Button>
       </Warp>
 
 
@@ -35,7 +39,7 @@ const Warp = styled.div`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url('/images/model-s.jpg');
+  background-image: ${props => `url("/images/${props.bgImage}")`};
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -51,6 +55,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
   display: flex;
   margin-bottom: 30px;
+  @media (max-width : 768px) {
+    flex-direction: column;
+  }
 `
 
 const LeftButton = styled.div`
@@ -66,14 +73,23 @@ const LeftButton = styled.div`
   text-transform: uppercase;
   font-size: 12px;
   cursor: pointer;
+  margin: 8px;
 
 `
 
 const RightButton = styled(LeftButton)`
+   background-color: white;
+   opacity: 0.65;
+   color: black;
+`
+
+const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
 
 `
 
-const DownArrow = styled.div`
-  margin-top: 20px;
-  height: 40px;
+const Button = styled.div`
+
 `
